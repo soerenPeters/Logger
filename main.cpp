32af1602fc521cffd882d86cdc120e8b70690445
Soeren Peters - iRMB <peters.soeren@gmx.net>
2017-07-17 14:19:40 +0200

    - extend example in main and add expected output file

--- main.cpp
+++ main.cpp
@@ -8,4 +8,13 @@ int main()
 	logging::Logger::setDebugLevel(logging::Logger::INTERMEDIATE);
 
 	logging::out << logging::Logger::LOW << "not printed\n" << logging::Logger::HIGH << "printed\n";
+
+	logging::Logger::makeLogger("stub");
+	logging::out << logging::Logger::HIGH << "not printed\n";
+
+	logging::Logger::makeLogger("file", "../log.txt");
+	logging::out << logging::Logger::HIGH << "printed to file...\n";
+
+	logging::Logger::makeLogger("console file", "../log.txt");
+	logging::out << logging::Logger::HIGH << "printed to file and console...\n";
 }
